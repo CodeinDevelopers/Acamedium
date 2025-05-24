21e26c7483f8203536567acd30dbd634bee57365
Akingbe Samuel <Samuelmayowa33@gmail.com>
2025-05-24 02:55:42 +0100

    update to home page

--- assets/js/core/system.js
+++ assets/js/core/system.js
@@ -1,20 +1,15 @@
 const statusButton = document.getElementById('statusButton');
-const linkUrl = "https://monitor.codeindevelopers.com.ng/s/codeindevelopers/"; // URL to navigate to when the button is clicked
-
-// Add click event listener to the button
+const linkUrl = "https://monitor.codeindevelopers.com.ng/s/codeindevelopers/"; 
 statusButton.addEventListener('click', function() {
-    window.location.href = linkUrl; // Redirect to the link
+    window.location.href = linkUrl; 
 });
-
-// Hover effect
 statusButton.addEventListener('mouseover', function() {
     statusButton.style.backgroundColor = '#e1e1e1';
     statusButton.style.transform = 'scale(1.05)';
-    statusButton.style.border = '0px solid #007ccc'; // Change border color on hover
+    statusButton.style.border = '0px solid #007ccc'; 
 });
-
 statusButton.addEventListener('mouseout', function() {
     statusButton.style.backgroundColor = '#f5f5f5';
     statusButton.style.transform = 'scale(1)';
-    statusButton.style.border = '0px solid #e1e1e1'; // Reset border color when not hovered
+    statusButton.style.border = '0px solid #e1e1e1'; 
 });
